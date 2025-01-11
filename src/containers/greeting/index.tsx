891853fb822b8c8041a65a203e533f138f3c4ef7
adrian <adrian.lpes@gmail.com>
2025-01-11 16:16:30 +0100

    feat: scalability refactor

--- src/containers/greeting/index.tsx
+++ src/containers/greeting/index.tsx
@@ -2,15 +2,12 @@
 import { Grid, Text, Widget } from '@/components';
 import styles from './style.module.scss';
 import Image from 'next/image';
-
-const ME = 'https://www.npmjs.com/npm-avatar/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXJVUkwiOiJodHRwczovL3MuZ3JhdmF0YXIuY29tL2F2YXRhci9jNmY3MzEyMDQwYjAxZGVmMTBhN2JlYmQ1ZjFiYjM1OT9zaXplPTQ5NiZkZWZhdWx0PXJldHJvIn0.xNYRPSHvhcS8ISzY5PID96v3j39uPLV2Od14wYgSDrs';
-const MIN_COL_WIDTH = 120;
-const SOCIAL_ICON_WIDTH = 16;
+import { SETTINGS } from './settings';
 
 const Photo = () => {
   return (
     <Widget className={styles.photo}>
-      <Image src={ME} width={100} height={100} alt='me' className={styles.image}/>
+      <Image src={SETTINGS.PROFILE_IMAGE} width={100} height={100} alt='me' className={styles.image} priority/>
     </Widget>
   );
 };
@@ -37,35 +34,26 @@ const Presentation = () => {
   );
 };
 
-type SocialMedia = 'instagram' | 'github' | 'linkedin' | 'mail';
-
-const SocialIcon = ({ media }: { media: SocialMedia }) => {
-  return (
-    <Grid.Item className={styles.socialIcon}>
-      <Image
-        style={{
-          filter: 'brightness(1.2) contrast(90%)',
-        }}
-        src={`/social/${media}.svg`}
-        height={SOCIAL_ICON_WIDTH}
-        width={SOCIAL_ICON_WIDTH}
-        alt='instagram'
-        className={styles.socialIconImage}
-      />
-    </Grid.Item>
-  );
-};
-
 const Social = () => {
   return (
     <Widget className={styles.social}>
       <Grid minWidth={40} gap={10} className={styles.socialGrid}>
-        <SocialIcon media='instagram'/>
-        <SocialIcon media='github'/>
-        <SocialIcon media='linkedin'/>
-          <a href='mailto:adrian.lpes@gmail.com'>
-          <SocialIcon media='mail' />
-        </a>
+        {Object.values(SETTINGS.SOCIAL).map(({ href, icon }) => {
+          return (
+            <Grid.Item className={styles.socialIcon} key={href}>
+              <a href={href} target='_blank'>
+                <Image
+                  style={{ filter: 'brightness(1.2) contrast(90%)' }}
+                  src={`/social/${icon}.svg`}
+                  height={SETTINGS.SOCIAL_ICON_WIDTH}
+                  width={SETTINGS.SOCIAL_ICON_WIDTH}
+                  alt={icon}
+                  className={styles.socialIconImage}
+                />
+              </a>
+            </Grid.Item>
+          )
+        })}
       </Grid>
     </Widget>
   );
@@ -73,7 +61,7 @@ const Social = () => {
 
 const Greeting = () => {
   return (
-    <Grid gap={5} minWidth={MIN_COL_WIDTH}>
+    <Grid gap={5} minWidth={SETTINGS.MIN_COLUMN_WIDTH}>
       <Grid.Item span={1}>
         <Photo />
       </Grid.Item>
---
+++ src/containers/greeting/settings.ts
@@ -0,0 +1,25 @@
+const SETTINGS = {
+  PROFILE_IMAGE: 'https://www.npmjs.com/npm-avatar/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXJVUkwiOiJodHRwczovL3MuZ3JhdmF0YXIuY29tL2F2YXRhci9jNmY3MzEyMDQwYjAxZGVmMTBhN2JlYmQ1ZjFiYjM1OT9zaXplPTQ5NiZkZWZhdWx0PXJldHJvIn0.xNYRPSHvhcS8ISzY5PID96v3j39uPLV2Od14wYgSDrs',
+  MIN_COLUMN_WIDTH: 120,
+  SOCIAL_ICON_WIDTH: 16,
+  SOCIAL: {
+    INSTAGRAM: {
+      href: 'https://www.instagram.com/adrihfly/',
+      icon: 'instagram',
+    },
+    LINKEDIN: {
+      href: 'https://www.linkedin.com/in/adrihfly/',
+      icon: 'linkedin',
+    },
+    GITHUB: {
+      href: 'https://github.com/adrihle/',
+      icon: 'github',
+    },
+    MAIL: {
+      href: 'mailto:adrian.lpes@gmail.com',
+      icon: 'mail',
+    },
+  }
+}
+
+export { SETTINGS };
