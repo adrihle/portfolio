c1670079e587b8416acac0ff3274c7759e2cdbfd
adrian <adrian.lpes@gmail.com>
2025-01-18 18:10:15 +0100

    fix: wipe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import '@/styles/global.scss';
 import { Grid, Page } from "@/components";
 import { Greeting, Navigation, LocaleSelector, Social } from "@/containers";
+import styles from './style.module.scss';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,16 +28,16 @@ export default async function RootLayout({ children, params }: any) {
         <Grid minWidth="220px" gap={15}>
           <Grid.Item span={1}>
             <Grid minWidth="220px" gap={15}>
-              <Grid.Item>
+              <Grid.Item className={styles.greeting}>
                 <Greeting />
               </Grid.Item>
-              <Grid.Item>
+              <Grid.Item className={styles.social}>
                 <Social />
               </Grid.Item>
-              <Grid.Item>
+              <Grid.Item className={styles.navigation}>
                 <Navigation />
               </Grid.Item>
-              <Grid.Item>
+              <Grid.Item className={styles.locale}>
                 <LocaleSelector />
               </Grid.Item>
             </Grid>
