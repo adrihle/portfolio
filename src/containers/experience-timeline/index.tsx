a14b8b83a7282b9fc951ea9b0f5faced25b0cf90
adrian <adrian.lpes@gmail.com>
2025-01-26 21:49:09 +0100

    fix: wipe

--- src/containers/experience-timeline/index.tsx
+++ src/containers/experience-timeline/index.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 'use client'
 import { useMemo } from 'react';
 import Image from 'next/image';
 import { IntersectionProvider, useIntersection } from "@adrihfly/intersection-hook";
 import { ProviderDate } from '@/providers';
 import { ComponentProps } from '@/interfaces';
-import { Card, Icon, Text } from '@/components';
+import { Card, Icon, List, Text } from '@/components';
 import { TECH_STACK } from '@/common';
 import styles from './style.module.scss';
+import Experience from '@/app/[locale]/experience/page';
+import React from 'react';
 
 type Experience = {
   company: string;
@@ -71,7 +74,18 @@ const Timeline = ({ experiences }: ExperienceTimelineProps) => {
   );
 };
 
-const Section = ({ company, id, position, from, to, description, stack, location, logo, ...props }: Experience & ComponentProps & { id: string }) => {
+const StackList = React.memo(function render({ stack }: { stack: Experience['stack'] }) {
+  return (
+    <List
+      list={stack}
+      renderElement={(icon) => <Icon.Brand icon={icon} className={styles.tech} />}
+      className={styles.stack}
+      delay={0.02}
+    />
+  );
+});
+
+const Section = React.memo(function render({ company, id, position, from, to, description, stack, location, logo, ...props }: Experience & ComponentProps & { id: string }) {
   const { register } = useIntersection();
   return (
     <Card
@@ -100,12 +114,10 @@ const Section = ({ company, id, position, from, to, description, stack, location
         <Icon src='location' />
         <Text size='small'>{location}</Text>
       </div>
-      <div className={styles.stack}>
-        {stack.map((i) => <Icon.Brand icon={i} key={i} className={styles.tech} />)}
-      </div>
+      <StackList stack={stack}/>
     </Card>
   );
-};
+});
 
 const Sections = ({ experiences }: { experiences: Record<string, Experience> }) => {
   const sortedExperiences = Object.entries(experiences)
