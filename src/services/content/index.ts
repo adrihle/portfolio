a1b3d746d65c2d203bbd11ae6d30b1465efe29f1
Adrian Lopez <adrian.lpes@gmail.com>
2025-04-09 17:34:32 +0200

    fix: mongo ...

--- src/services/content/index.ts
+++ src/services/content/index.ts
@@ -3,7 +3,7 @@ import { Locale } from "@/interfaces";
 import { ProviderAI } from '@/providers/ai';
 import { ProviderLog } from '@/providers/log';
 import { APP_SETTINGS } from "@/settings";
-import { RepositoryTranslates } from "@/repositories";
+//import { RepositoryTranslates } from "@/repositories";
 import { getLastFileUpdate } from "@/utils/getLastFileUpdate";
 import { ProviderDate } from "@/providers/date";
 import * as helpers from './helpers';
@@ -20,45 +20,45 @@ type TextPage<T> = {
 const generatePageTexts = async <T>({ page, locale, text }: TextPage<T>) => {
   if (locale === APP_SETTINGS.DEFAULT_LOCALE) return text;
 
-  logger.debug(`Fetching content for ${locale} of page: ${page}`);
-  const translation = await RepositoryTranslates.get({ page, locale });
-  if (translation) return translation;
-
-  const generated = await ProviderAI.translateText({ locale, text });
-  if (!generated) return text;
-
-  logger.debug(`Translate successfull content for ${locale} of page: ${page}`);
-  await RepositoryTranslates.write({ page, locale, translations: generated });
-  return generated;
+  // logger.debug(`Fetching content for ${locale} of page: ${page}`);
+  // const translation = await RepositoryTranslates.get({ page, locale });
+  // if (translation) return translation;
+  //
+  // const generated = await ProviderAI.translateText({ locale, text });
+  // if (!generated) return text;
+  //
+  // logger.debug(`Translate successfull content for ${locale} of page: ${page}`);
+  // await RepositoryTranslates.write({ page, locale, translations: generated });
+  // return generated;
 };
 
 const updatePageTexts = async <T>({ page, locale, text, filePath }: TextPage<T> & { filePath: string }) => {
   logger.debug(`Updating translations ${page} | ${locale}`);
   if (locale === APP_SETTINGS.DEFAULT_LOCALE) return text;
 
-  const lastUpdate = await getLastFileUpdate({ filePath });
-  if (!lastUpdate) return;
-
-  const formattedLastFileUpdate = ProviderDate.format({ date: lastUpdate, format: 'yyyy-MM-dd' })
-
-  const existsDocument = await RepositoryTranslates.exists({ page, locale });
-  const lastTranslationUpdate = await RepositoryTranslates.getLastUpdate({ page, locale });
-
-  const isUpdated = lastTranslationUpdate ? ProviderDate.isBefore(formattedLastFileUpdate, lastTranslationUpdate) : true;
-
-  if (isUpdated && existsDocument) {
-    logger.debug(`Translates ${page} | ${locale} is up to date`);
-    return;
-  };
-
-  logger.info(`Updating ${page} | ${locale}`);
-
-  const generated = await ProviderAI.translateText({ locale, text });
-  if (!generated) return text;
-
-  logger.debug(`Translate successfull content for ${locale} of page: ${page}`);
-  await RepositoryTranslates.update({ page, locale, translations: generated });
-  logger.debug(`Successfull stored ${page} | ${locale}`);
+  // const lastUpdate = await getLastFileUpdate({ filePath });
+  // if (!lastUpdate) return;
+  //
+  // const formattedLastFileUpdate = ProviderDate.format({ date: lastUpdate, format: 'yyyy-MM-dd' })
+  //
+  // const existsDocument = await RepositoryTranslates.exists({ page, locale });
+  // const lastTranslationUpdate = await RepositoryTranslates.getLastUpdate({ page, locale });
+  //
+  // const isUpdated = lastTranslationUpdate ? ProviderDate.isBefore(formattedLastFileUpdate, lastTranslationUpdate) : true;
+  //
+  // if (isUpdated && existsDocument) {
+  //   logger.debug(`Translates ${page} | ${locale} is up to date`);
+  //   return;
+  // };
+  //
+  // logger.info(`Updating ${page} | ${locale}`);
+  //
+  // const generated = await ProviderAI.translateText({ locale, text });
+  // if (!generated) return text;
+  //
+  // logger.debug(`Translate successfull content for ${locale} of page: ${page}`);
+  // await RepositoryTranslates.update({ page, locale, translations: generated });
+  // logger.debug(`Successfull stored ${page} | ${locale}`);
 };
 
 const ServiceContent = {
