76213c1c7f0398b8e55af72ea640e5f68b7af0c6
Adrian Lopez <adrian.lpes@gmail.com>
2025-05-11 17:38:49 +0200

    fix: remove temporary cache connection

--- src/app/[locale]/contributions/action.ts
+++ src/app/[locale]/contributions/action.ts
@@ -1,49 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { PackageInfo } from '@/containers';
 import { ServiceContent } from '@/services';
-import { exec } from 'child_process';
-import util from 'util';
 import { CONTRIBUTION_PAGE, ContributionPage } from './settings';
 import { Locale } from '@/interfaces';
-import { ProviderCache } from '@/providers/cache';
-import { ProviderLog } from '@/providers/log';
-
-const logger = new ProviderLog('NPM');
-const execPromise = util.promisify(exec);
-const KEY = 'root#packages';
-const TTL = 60 * 60 * 24 * 1;
-
-type NPMPackage = {
-  name: string;
-  links: { npm: string, homepage: string };
-  date: string;
-  license: string;
-  version: string;
-  description: string;
-};
-
-const getContributions = async (): Promise<PackageInfo[]> => {
-  try {
-    const cached = await ProviderCache.get<PackageInfo[]>({ key: KEY });
-    if (cached) return cached;
-    const { stdout } = await execPromise('npm search @adrihfly --json');
-    const result = JSON.parse(stdout) as NPMPackage[];
-    const packages = result.map(({ name, links, date, license, version, description }) => ({
-      name,
-      href: links.npm,
-      homepage: links.homepage,
-      date,
-      license,
-      version,
-      description,
-    }));
-    await ProviderCache.set({ key: KEY, value: packages, expire: TTL });
-    return packages;
-  } catch (error) {
-    logger.error(`Error fetching npm contributions`, { error });
-    return [];
-  }
-}
 
 const getContent = async (locale: Locale): Promise<ContributionPage> => {
   const { PACKAGES, ...rest } = CONTRIBUTION_PAGE;
@@ -64,4 +22,4 @@ const updateContent = async (locale: Locale) => {
   await ServiceContent.updatePageTexts({ page: 'contributions', locale, text: rest, filePath });
 };
 
-export { getContributions, getContent, updateContent };
+export { getContent, updateContent };
